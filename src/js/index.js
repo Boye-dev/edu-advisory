cef9b1b601f3fae29e8e90fc4f776d47f6efc41d
Boye-dev <oyelolaboye@gmail.com>
2024-09-03 20:15:33 +0100

    feat: send feedback

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,21 @@ sideBarHamburger.addEventListener("click", () => {
   sideBarHamburger.classList.toggle("fa-bars");
   sidebar.classList.toggle("active_navbar");
 });
+
+const feedbackButton = document.getElementById("feedbackButton");
+const feedbackModal = document.getElementById("feedbackModal");
+const closeButton = document.getElementById("closeButton");
+
+feedbackButton.onclick = function () {
+  feedbackModal.style.display = "flex";
+};
+
+closeButton.onclick = function () {
+  feedbackModal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target === feedbackModal) {
+    feedbackModal.style.display = "none";
+  }
+};
